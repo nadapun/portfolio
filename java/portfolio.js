2788f2872afed560b59ea4cf0a824122fbd08613
Andy Springer <andy.springer@zy.cdut.edu.cn>
2025-04-11 00:02:43 +0800

    portfolio fixes?

--- java/portfolio.js
+++ java/portfolio.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
   const viewerImage = document.querySelector('.viewer img');
 
   thumbnails.forEach(thumbnail => {
+    // Update the image in the viewer when a thumbnail is clicked
     thumbnail.addEventListener('click', function() {
+      const imagePath = this.getAttribute('data-src');
+      viewerImage.src = imagePath;
+    });
+
+    // For mobile devices, also add touch event handling
+    thumbnail.addEventListener('touchstart', function() {
       const imagePath = this.getAttribute('data-src');
       viewerImage.src = imagePath;
     });
