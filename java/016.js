2419fb6f56152752113a76fb8aa7e68b2f975400
Andy Springer <andy.springer@zy.cdut.edu.cn>
2025-04-10 21:04:23 +0800

    made page 16 bee interaction

---
+++ java/016.js
@@ -0,0 +1,170 @@
+// Drawing with text. Ported from Generative Design book - http://www.generative-gestaltung.de - Original licence: http://www.apache.org/licenses/LICENSE-2.0
+
+// Application variables
+var position = {x: 0, y: window.innerHeight/2};
+var counter = 0;
+var minFontSize = 3;
+var angleDistortion = 0;
+var hasDrawn = false;
+var letters = "poem ";
+
+// Drawing variables
+var canvas;
+var context;
+var mouse = {x: 0, y: 0, down: false}
+var touch = {x: 0, y: 0, down: false}
+
+function init() {
+  canvas = document.getElementById( 'canvas' );
+  context = canvas.getContext( '2d' );
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  drawTitle();
+  
+  canvas.addEventListener('mousemove', mouseMove, false);
+  canvas.addEventListener('mousedown', mouseDown, false);
+  canvas.addEventListener('mouseup',   mouseUp,   false);
+  canvas.addEventListener('mouseout',  mouseUp,  false);  
+  canvas.addEventListener('dblclick', doubleClick, false);
+
+  canvas.addEventListener('touchstart', handleTouchStart, false);
+  canvas.addEventListener('touchmove', handleTouchMove, false);
+  canvas.addEventListener('touchend', handleTouchEnd, false);
+  
+  window.onresize = function(event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+}
+
+function drawTitle() {
+  context.save();
+  context.font = '48px Josefin Slab, serif';
+  context.fillStyle = '#3a3a3a';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle'; // important for vertical centering
+  context.fillText('Click  Draw  Bzzz', canvas.width / 2, canvas.height / 2);
+  context.restore();
+}
+
+
+function mouseMove ( event ){
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+  draw();
+}
+
+function updateBeePosition(x, y) {
+  const bee = document.getElementById('bee-follow');
+  bee.style.left = (x - 15) + 'px'; // Center bee
+  bee.style.top = (y - 15) + 'px';
+  bee.style.display = 'block';
+}
+
+
+function draw() {
+  if (!mouse.down) return;
+
+  // On first draw, clear title
+  if (!hasDrawn) {
+    hasDrawn = true;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  // Calculate distance between last and current point
+  var d = distance(position, mouse);
+  var fontSize = minFontSize + d / 2 + 7;
+  var letter = letters[counter];
+  var stepSize = textWidth(letter, fontSize);
+
+  // Only draw if the mouse moved far enough
+  if (d > stepSize) {
+    var angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
+    context.font = fontSize + "px Georgia";
+
+    context.save();
+    context.translate(position.x + 12, position.y + 25);
+    context.rotate(angle);
+    context.fillText(letter, 0, 0);
+    context.restore();
+
+    // Move to next letter
+    counter++;
+    if (counter >= letters.length) {
+      counter = 0;
+    }
+
+    // Update drawing position
+    position.x += Math.cos(angle) * stepSize;
+    position.y += Math.sin(angle) * stepSize;
+  }
+}
+
+
+function handleTouchStart(e) {
+  mouse.down = true;
+  const touch = e.touches[0];
+  position.x = touch.pageX;
+  position.y = touch.pageY;
+  document.getElementById('info').style.display = 'none';
+  e.preventDefault(); // Prevent scrolling
+}
+
+function handleTouchMove(e) {
+  if (!mouse.down) return;
+  const touch = e.touches[0];
+  mouse.x = touch.pageX;
+  mouse.y = touch.pageY;
+  updateBeePosition(mouse.x, mouse.y);
+  draw();
+  e.preventDefault(); // Prevent scrolling
+}
+
+function handleTouchEnd(e) {
+  mouse.down = false;
+}
+
+function distance( pt, pt2 ){
+  
+  var xs = 0;
+  var ys = 0;
+ 
+  xs = pt2.x - pt.x;
+  xs = xs * xs;
+ 
+  ys = pt2.y - pt.y;
+  ys = ys * ys;
+ 
+  return Math.sqrt( xs + ys );
+}
+
+function mouseDown( event ){
+  mouse.down = true;
+  position.x = event.pageX;
+  position.y = event.pageY;
+  
+  document.getElementById('info').style.display = 'none';
+}
+
+function mouseUp( event ){
+    mouse.down = false;
+}
+
+function doubleClick( event ) {
+  canvas.width = canvas.width; 
+}
+
+function textWidth( string, size ) {
+  context.font = size + "px Georgia";
+  context.fillStyle = '#3a3a3a';
+  
+  if ( context.fillText ) {
+    return context.measureText( string ).width;
+  } else if ( context.mozDrawText) {
+    return context.mozMeasureText( string );
+  }
+  
+ };
+
+init();
